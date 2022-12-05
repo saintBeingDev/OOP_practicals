13a2f173eff1f0a73fd8f9e432ce0ed6fb9699e6
Om Nikam <omnikam0404@gmail.com>
2022-12-05 19:50:29 +0530

    Assignment05 edited

--- Assignment05/Main.java
+++ Assignment05/Main.java
@@ -1,4 +1,5 @@
 package Assignment05;
+import java.util.Scanner;
 /*
 Design and develop a context for given case study and implement an interface for Vehicles
 Consider the example of vehicles like bicycle, car and bike. All Vehicles have common
@@ -9,24 +10,39 @@ functionalities in their own class in their own way
  */
 public class Main {
     public static void main(String[] args) {
-        Bicycle bicycle1 = new Bicycle();
-        bicycle1.gearChange(2);
-        bicycle1.speedUp(10);
-        bicycle1.applyBreak(5);
-        bicycle1.display();
-
-        System.out.println("-----------------------------------------");
-        Car car1 = new Car();
-        car1.display();
-        car1.gearChange(3);
-        car1.speedUp(20);
-        car1.applyBreak(10);
-
-        System.out.println("-----------------------------------------");
-        Bike bike1 = new Bike() ;
-        bike1.display();
-        bike1.gearChange(3);
-        bike1.speedUp(20);
-        bike1.applyBreak(10);
+        int flag = 0;
+        Scanner sc = new Scanner(System.in);
+        while(flag != -1){
+            System.out.println("Enter 1 for Bicycle");
+            System.out.println("Enter 2 for Car");
+            System.out.println("Enter 3 for Bike");
+            int choice;
+            choice = sc.nextInt();
+            switch(choice){
+                case 1 ->{
+                    Bicycle bicycle1 = new Bicycle();
+                    bicycle1.gearChange(2);
+                    bicycle1.speedUp(10);
+                    bicycle1.applyBreak(5);
+                    bicycle1.display();
+                }
+                case 2 ->{
+                    Car car1 = new Car();
+                    car1.display();
+                    car1.gearChange(3);
+                    car1.speedUp(20);
+                    car1.applyBreak(10);
+                }
+                case 3 ->{
+                    Bike bike1 = new Bike() ;
+                    bike1.display();
+                    bike1.gearChange(3);
+                    bike1.speedUp(20);
+                    bike1.applyBreak(10);
+                }
+            }
+            System.out.println("Press -1 to Exit and press any other number to continue");
+            flag = sc.nextInt();
+        }
     }
 }
